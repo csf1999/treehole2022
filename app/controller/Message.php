372734a1802d7316771656797d6f99004d9d7933
csf1999 <99321689+csf1999@users.noreply.github.com>
2022-02-22 10:57:18 +0800

    Update Message.php
    tp6 Message User

--- app/controller/Message.php
+++ app/controller/Message.php
@@ -1,68 +1,67 @@
 <?php
 namespace app\controller;
+use think\Facade\Db;
+use think\Facade\Request;
 
-//use Think\Controller;
 class Message {
     //发布新树洞
     public function publish_new_message(){
         //校验参数是否存在
-        if(!$_POST['user_id']){
+        if(!Request::post('user_id')){
             $return_data = array();
             $return_data['error_code'] = 1;
             $return_data['msg'] = '参数不足:user_id';
-            $this->ajaxReturn($return_data);
+            return json($return_data);
         }
-        if(!$_POST['username']){
+        if(!Request::post('username')){
             $return_data = array();
             $return_data['error_code'] = 1;
             $return_data['msg'] = '参数不足:username';
-            $this->ajaxReturn($return_data);
+            return json($return_data);
         }
-        if(!$_POST['face_url']){
+        if(!Request::post('face_url')){
             $return_data = array();
             $return_data['error_code'] = 1;
             $return_data['msg'] = '参数不足:face_url';
-            $this->ajaxReturn($return_data);
+            return json($return_data);
         }
-        if(!$_POST['content']){
+        if(!Request::post('content')){
             $return_data = array();
             $return_data['error_code'] = 1;
             $return_data['msg'] = '参数不足:content';
-            $this->ajaxReturn($return_data);
+            return json($return_data);
         }
 
-        $Message = M('Message');
+        $Message = Db::name('Message');
         //设置要插入的数据
         $data = array();
-        $data['user_id'] = $_POST['user_id'];
-        $data['username'] = $_POST['username'];
-        $data['face_url'] = $_POST['face_url'];
-        $data['content'] = $_POST['content'];
+        $data['user_id'] = Request::post('user_id');
+        $data['username'] = Request::post('username');
+        $data['face_url'] = Request::post('face_url');
+        $data['content'] = Request::post('content');
         $data['total_likes'] = 0;
         $data['send_timestamp'] = time();
 
-        $result = $Message->add($data);
+        $result = $Message->insert($data);
 
         if($result){
             $return_data = array();
             $return_data['error_code'] = 0;
             $return_data['msg'] = '数据添加成功';
-            $this->ajaxReturn($return_data);
+            return json($return_data);
         }
         else{
             $return_data = array();
             $return_data['error_code'] = 2;
             $return_data['msg'] = '数据添加失败';
-            $this->ajaxReturn($return_data);
+            return json($return_data);
         }
-
-        dump($_POST);
     }
 
     //得到所有人的树洞
     public function get_all_messages(){
-        $Message = M('Message');
-        $all_messages = $Message->order('id desc')->select();
+        $Message = Db::name('Message');
+        $all_messages = $Message->order('id desc')->select()->toArray();
         //dump($all_messages);
         foreach($all_messages as $key => $message){
             $all_messages[$key]['send_timestamp'] = date('y-m-d h:i:s',$message['send_timestamp']);
@@ -71,24 +70,24 @@ class Message {
         $return_data['error_code'] = 0;
         $return_data['msg'] = '数据获取成功';
         $return_data['data'] = $all_messages;
-        $this->ajaxReturn($return_data);
+        return json($return_data);
         //dump($all_messages);
     }
 
     //得到指定人的树洞
     public function get_one_user_all_messages(){
         
-        if(!$_POST['user_id']){
+        if(!Request::post('user_id')){
             $return_data = array();
             $return_data['error_code'] = 1;
             $return_data['msg'] = '参数不足:user_id';
-            $this->ajaxReturn($return_data);
+            return json($return_data);
         }
         else{
-            $Message = M('Message');
+            $Message = Db::name('Message');
             $where = array();
-            $where['user_id'] = $_POST['user_id'];
-            $all_messages = $Message->where($where)->order('id desc')->select();
+            $where['user_id'] = Request::post('user_id');
+            $all_messages = $Message->where($where)->order('id desc')->select()->toArray();
             // dump(all_messages);
             foreach($all_messages as $key => $message){
                 $all_messages[$key]['send_timestamp'] = date('y-m-d h:i:s',$message['send_timestamp']);
@@ -97,93 +96,93 @@ class Message {
             $return_data['error_code'] = 0;
             $return_data['msg'] = '数据获取成功';
             $return_data['data'] = $all_messages;
-            $this->ajaxReturn($return_data);
+            return json($return_data);
         }
     }
 
     //点赞
     public function do_like(){
-        if(!$_POST['message_id']){
+        if(!Request::post('message_id')){
             $return_data = array();
             $return_data['error_code'] = 1;
             $return_data['msg'] = '参数不足:message_id';
-            $this->ajaxReturn($return_data);
+            return json($return_data);
         }
-        if(!$_POST['user_id']){
+        if(!Request::post('user_id')){
             $return_data = array();
             $return_data['error_code'] = 1;
             $return_data['msg'] = '参数不足:user_id';
-            $this->ajaxReturn($return_data);
+            return json($return_data);
         }
         //dump($_POST);
-        $Message = M('Message');
+        $Message = Db::name('Message');
         $where = array();
-        $where['id'] = $_POST['message_id'];
+        $where['id'] = Request::post('message_id');
         $message = $Message->where($where)->find();
 
         if(!$message){
             $return_data = array();
             $return_data['error_code'] = 2;
             $return_data['msg'] = '指定的树洞不存在';
-            $this->ajaxReturn($return_data);
+            return json($return_data);
         }
         //dump($message);
         
         $data = array();
         $data['total_likes'] = $message['total_likes'] + 1;
         
-        // $where = array();
-        // $where['id'] = $_POST['message_id'];
+        $where = array();
+        $where['id'] = Request::post('message_id');
         $result = $Message->where($where)->save($data);
 
         if($result){
             $return_data = array();
             $return_data['error_code'] = 0;
             $return_data['msg'] = '数据保存成功';
-            $return_data['data']['message_id'] = $_POST['message_id'];
+            $return_data['data']['message_id'] = Request::post('message_id');
             $return_data['data']['total_likes'] = $data['total_likes'];
-            $this->ajaxReturn($return_data);
+            return json($return_data);
         }
         else{
             $return_data = array();
             $return_data['error_code'] = 2;
             $return_data['msg'] = '数据保存失败';
-            $this->ajaxReturn($return_data);
+            return json($return_data);
         }
     }
 
     //删除指定树洞
     public function delete_message(){
-        if(!$_POST['message_id']){
+        if(!Request::post('message_id')){
             $return_data = array();
             $return_data['error_code'] = 1;
             $return_data['msg'] = '参数不足:message_id';
-            $this->ajaxReturn($return_data);
+            return json($return_data);
         }
-        if(!$_POST['user_id']){
+        if(!Request::post('user_id')){
             $return_data = array();
             $return_data['error_code'] = 1;
             $return_data['msg'] = '参数不足:user_id';
-            $this->ajaxReturn($return_data);
+            return json($return_data);
         }
 
-        $Message = M('Message');
+        $Message = Db::name('Message');
         $where = array();
-        $where['id'] = $_POST['message_id'];
+        $where['id'] = Request::post('message_id');
         $message = $Message->where($where)->delete();
 
         if(!$message){
             $return_data = array();
             $return_data['error_code'] = 2;
             $return_data['msg'] = '指定的树洞不存在';
-            $this->ajaxReturn($return_data);
+            return json($return_data);
         }
         else{
             $return_data = array();
             $return_data['error_code'] = 0;
             $return_data['msg'] = '数据删除成功';
-            $return_data['data']['message_id'] = $_POST['message_id'];
-            $this->ajaxReturn($return_data);
+            $return_data['data']['message_id'] = Request::post('message_id');
+            return json($return_data);
         }
     }
 }
