cbcf5f10ecb5944494902a1ff5a0276aa5021a44
csf1999 <99321689+csf1999@users.noreply.github.com>
2022-02-24 10:16:31 +0800

    Update User.php
    id user_id

--- app/controller/User.php
+++ app/controller/User.php
@@ -64,7 +64,7 @@ class User {
                 $return_data = array();
                 $return_data['error_code'] = 0;
                 $return_data['msg'] = '注册成功';
-                $return_data['data']['user_id'] = $result;
+                $return_data['data']['user_id'] = Request::post('id');
                 $return_data['data']['username'] = Request::post('username');
                 $return_data['data']['phone'] = Request::post('phone');
                 $return_data['data']['password'] = Request::post('password');
